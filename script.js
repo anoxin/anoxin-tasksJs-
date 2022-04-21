320ef98319467f7a9785237889ab3e7dc0c08140
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-04-21 11:16:52 +0300

    исправление ошибок

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-let num;
-let sum;
-
-function one() {
-    num = Math.floor (Math.random () * (100 - 0 + 1)) + 0;
-    sum = 10;
+function startApp(num) {
+    let sum = 10;
+    let victory = false;
+    let answer = false;
     let myNum = prompt('Угадай число от 1 до 100!!! У вас есть ' + sum + ' попыток');
-    
-    function two() {
+
+    function comparingNumber() {
         sum--;
         console.log(myNum);
         if (sum < 0) {
-            let answer = confirm( 'Попытки закончились, хотите сыграть еще?');
-            if (answer == true) {
-                one(num);
-            }
-
+            answer = confirm( 'Попытки закончились, хотите сыграть еще?');
         } else if (myNum == null) {
             alert('Игра закончена');
         } else if (myNum == '' || myNum == undefined) {
             sum++;
             myNum = prompt('Введи число! Осталось ' + sum + ' попыток');
-            two();
+            comparingNumber();
         } else {
             myNum = +myNum;
             if (num < myNum) {
                 myNum = prompt('Загаданное число меньше! Осталось ' + sum + ' попыток');
-                two(num);
+                comparingNumber(num);
             } else if (num > myNum) {
                 myNum = prompt('Загаданное число больше Осталось ' + sum + ' попыток');
-                two(num);
+                comparingNumber(num);
             } else if (num === myNum) {
-                let victory = confirm('Поздравляю, Вы угадали число ' + num + '!!! Хотели бы сыграть еще?');
-                if (victory == true) {
-                    one(num);
-                }
+                victory = confirm('Поздравляю, Вы угадали число ' + num + '!!! Хотели бы сыграть еще?');
             } else {
-                myNum = prompt('Введи число! Осталось' + sum + ' попыток');
                 sum++;
-                two(num);
+                myNum = prompt('Введи число! Осталось' + sum + ' попыток');
+                comparingNumber(num);
             }
 
         }
         
     }
-    two();
+    comparingNumber();
+    if (victory == true || answer == true) {
+        startApp(num);
+    }
 }
 
 
-one(num);
+startApp(Math.floor (Math.random () * (100 - 0 + 1)) + 0);
