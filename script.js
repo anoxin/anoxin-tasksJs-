f9fd4b903682db471751a251887d1025c9273b88
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-05-03 14:17:52 +0300

    выполнение усложненного задания 1 урока №19

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
-function startApp() {
-    const num = Math.floor (Math.random () * (100 - 0 + 1)) + 0;
-    let sum = 10;
-    let victory = false;
-    let answer = false;
-    let myNum = prompt('Угадай число от 1 до 100!!! У вас есть ' + sum + ' попыток');
+const input = document.querySelector('input');
+const text = document.querySelector('p');
+let timeShow;
+let data = 0;
 
-    function comparingNumber() {
-        sum--;
-        if (sum < 0) {
-            answer = confirm( 'Попытки закончились, хотите сыграть еще?');
-            return;
-        } else if (myNum == null) {
-            alert('Игра закончена');
-            return;
-        } else if (myNum == '' || isNaN(myNum)) {
-            sum++;
-            myNum = prompt('Введи число! Осталось ' + sum + ' попыток');
-        } else {
-            if (num == myNum) {
-                victory = confirm('Поздравляю, Вы угадали число ' + num + '!!! Хотели бы сыграть еще?');
-                return victory;
-            } else if (Math.max(num, +myNum) == num) {
-                myNum = prompt('Загаданное число больше! Осталось ' + sum + ' попыток');
-            } else {
-                myNum = prompt('Загаданное число меньше! Осталось ' + sum + ' попыток');
-            }  
-        }
-        comparingNumber();
-        
-    }
-    comparingNumber();
-    if (victory == true || answer == true) {
-        startApp();
-    }
-}
+function debounce(fn, ms) {
+        return (args) => {
+            clearTimeout(timeShow);
+            timeShow = setTimeout(() => {
+                    fn.apply(this, args);
+                    console.log(data);
+                }, ms);
+        };
+  }
 
-startApp();
-
-
+input.addEventListener('input', debounce(() => {
+    text.textContent = input.value;
+}, 300));
